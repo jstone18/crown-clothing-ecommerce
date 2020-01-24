aa3444386225c242ea1a1aaeb2256834d644051a
jstone18 <jason.stone0808@gmail.com>
2020-01-24 11:19:07 -0500

    Create signin and signup page and import into App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import Shop from "./pages/shop/Shop";
 import Header from "./components/header/Header";
+import SignInSignUp from "./pages/signIn-signUp/SignInSignUp";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={Shop} />
+				<Route path="/signin" component={SignInSignUp} />
 			</Switch>
 		</div>
 	);
