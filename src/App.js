eab3876c6579f5ee4e65e33e8ccf99e141c886e8
jstone18 <jason.stone0808@gmail.com>
2020-01-24 10:52:42 -0500

    Update app with Header component and styling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import Shop from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={Shop} />
