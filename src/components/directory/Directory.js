2122863fcf79469a6da337c1928da25bbb1bc8e8
jstone18 <jason.stone0808@gmail.com>
2020-01-23 23:22:44 -0500

    Install React-Router-Dom. Update menu-items to navigate withRouter HOC.

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -12,32 +12,37 @@ class Directory extends Component {
 					title: "hats",
 					imageUrl:
 						"https://i.ibb.co/wKVPFK1/kaycee-ingram-4-EWh-ZW7q1hg-unsplash.jpg",
-					id: 1
+					id: 1,
+					linkUrl: "hats"
 				},
 				{
 					title: "jackets",
 					imageUrl:
 						"https://i.ibb.co/vZ9Fs2j/ethan-kent-w-W9lz-em-Jw-unsplash.jpg",
-					id: 2
+					id: 2,
+					linkUrl: ""
 				},
 				{
 					title: "sneakers",
 					imageUrl: "https://i.ibb.co/fYhtkX0/school-aden-2284257-1920.jpg",
-					id: 3
+					id: 3,
+					linkUrl: ""
 				},
 				{
 					title: "womens",
 					imageUrl:
 						"https://i.ibb.co/W2qvPzj/jyotirmoy-gupta-rfam-Wy-Yn-Zm-I-unsplash.jpg",
 					size: "large",
-					id: 4
+					id: 4,
+					linkUrl: ""
 				},
 				{
 					title: "mens",
 					imageUrl:
 						"https://i.ibb.co/BfYpzfD/aj-gallagher-ep-Nph-Ijd-F84-unsplash.jpg",
 					size: "large",
-					id: 5
+					id: 5,
+					linkUrl: ""
 				}
 			]
 		};
@@ -45,8 +50,8 @@ class Directory extends Component {
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ title, imageUrl, id, size }) => (
-					<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+				{this.state.sections.map(({ id, ...otherSectionProps }) => (
+					<MenuItem key={id} {...otherSectionProps} />
 				))}
 			</div>
 		);
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./homePage.scss";
 import Directory from "../../components/directory/Directory";
 
-const HomePage = () => {
+const HomePage = ({ history }) => {
 	return (
 		<div className="homepage">
 			<Directory />
