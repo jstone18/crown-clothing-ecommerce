a412ada7f819d93e44620a78721b4098cebe343f
jstone18 <jason.stone0808@gmail.com>
2020-01-24 22:19:09 -0500

    Update header with sign in or sign out based on user state. Add styling to google custom button

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -49,11 +49,12 @@ export default class SignIn extends Component {
 						handleInputChange={this.handleInputChange}
 						label="password"
 					/>
-
-					<CustomButton type="submit">Sign In</CustomButton>
-					<CustomButton onClick={signInWithGoogle}>
-						Sign In With Google
-					</CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit">Sign In</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign In With Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
