42930dc513b8a67e8613c8f0b95e8b329713bd6f
jstone18 <jason.stone0808@gmail.com>
2020-01-25 20:23:51 -0500

    Remove uneended redux code

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./cartIcon.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { connect } from "react-redux";
+import { toggleShowCart } from "../../redux/actions/cartActions";
 
-const CartIcon = () => {
+const CartIcon = ({ toggleShowCart }) => {
 	return (
-		<div className="cart-icon">
+		<div className="cart-icon" onClick={toggleShowCart}>
 			<ShoppingIcon className="shopping-icon" />
 			<span className="item-count">0</span>
 		</div>
 	);
 };
 
-export default CartIcon;
+export default connect(null, { toggleShowCart })(CartIcon);
