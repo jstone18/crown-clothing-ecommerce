f7c97194c24afb36d8fd527ac32e96fc4549efff
jstone18 <jason.stone0808@gmail.com>
2020-02-03 22:47:59 -0500

    Update and normalize the shop data

--- src/redux/selectors/shop.selector.js
+++ src/redux/selectors/shop.selector.js
@@ -1,14 +1,5 @@
 import { createSelector } from "reselect";
 
-// Object to match string value to id of the collection
-const COLLECTION_ID_MAP = {
-	hats: 1,
-	sneakers: 2,
-	jackets: 3,
-	womens: 4,
-	mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -21,8 +12,5 @@ export const selectCollection = collectionUrlParam =>
 	createSelector(
 		[selectShopCollections],
 		// find collection.id matching the collectionUrlParam of the COLLECTION_ID_MAP
-		collections =>
-			collections.find(
-				collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-			)
+		collections => collections[collectionUrlParam]
 	);
