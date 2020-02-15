24ff8cb19a9fb0aab4d3843b3c2e11be15bb3263
jstone18 <jason.stone0808@gmail.com>
2020-02-15 00:37:31 -0500

    Refactor code with cart sagas (working)

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,8 @@
 import { all, call } from "redux-saga/effects";
-import { fetchCollectionsStart } from "../redux/sagas/shop.sagas";
+import { shopSagas } from "../redux/sagas/shop.sagas";
 import { userSagas } from "./sagas/user.sagas";
+import { cartSagas } from "./sagas/cart.sagas";
 
 export function* rootSaga() {
-	yield all([call(fetchCollectionsStart), call(userSagas)]);
+	yield all([call(shopSagas), call(userSagas), call(cartSagas)]);
 }
